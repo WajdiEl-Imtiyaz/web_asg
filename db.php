ebca8ab28e714858b7651711d3917935ba27c79e
Danial-n <danial.aminorazmi@gmail.com>
2025-10-29 18:13:14 +0800

    established db and login function

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '1234';
+$dbname = 'wd1_asg';
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
