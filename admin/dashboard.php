182d7c173a390a96a4b542b2ca9954cc78b07f9c
WajdiEl-Imtiyaz <21ftt1294@student.pb.edu.bn>
2025-10-31 00:16:15 +0800

    moved admin css into admin folder

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,7 +12,7 @@ if(empty($_SESSION['user']) || empty($_SESSION['is_admin'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link rel="stylesheet" href="../assets/styles.css">
+    <link rel="stylesheet" href="admin.css">
 </head>
 <body>
 
